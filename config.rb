763cd523e9e0c0b8e233cf531ac0bcd140f0e694
Niki Liu <nishiki.liu@gmail.com>
2013-08-03 13:07:40 -0400

    git status

--- config.rb
+++ config.rb
@@ -5,6 +5,10 @@
 activate :livereload
 activate :directory_indexes
 
+# Correct asset links on gh-pages
+activate :relative_assets
+set :relative_links, true
+
 set :css_dir,    'stylesheets'
 set :js_dir,     'javascripts'
 set :images_dir, 'images'
